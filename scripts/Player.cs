925779e4c18b28454085bc3d51d203e95fa0d609
mindcool24 <mindcool@mindcool.xyz>
2026-04-01 18:52:00 -0600

    Player controller go brr

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -9,4 +9,16 @@ public partial class Player : CharacterBody2D
 	public float JumpVelocity = 1000.0f;
 	[Export]
 	public float Gravity = 4000.0f;
+
+	[Export]
+	public Node2D Gun = null;
+
+	// TODO: Move this from here to somewhere better (probably into the states)
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion eventMouseMotion)
+		{
+			Gun.LookAt(GetGlobalMousePosition());
+		}
+	}
 }
