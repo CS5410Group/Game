ac11a718b1fea2d1b05bb4d7011b8e21f69cf5ff
mindcool24 <mindcool@mindcool.xyz>
2026-04-08 23:41:13 -0600

    fixed double jump bug

--- scripts/statemachine/playerstates/PJumpState.cs
+++ scripts/statemachine/playerstates/PJumpState.cs
@@ -9,6 +9,7 @@ public partial class PJumpState : PlayerState
 		Vector2 new_vel = Vector2.Zero;
 		new_vel.Y = -player.JumpVelocity;
 		player.Velocity = new_vel;
+		player.coyote = false;
 		// Play jump animation
 
 	}
