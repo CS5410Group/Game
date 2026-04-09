d9d69fb2c417f432f7a0302da3883fb837f62705
mindcool24 <mindcool@mindcool.xyz>
2026-04-08 22:30:20 -0600

    removed todos

--- scripts/statemachine/playerstates/PIdleState.cs
+++ scripts/statemachine/playerstates/PIdleState.cs
@@ -4,11 +4,6 @@ using System;
 [GlobalClass]
 public partial class PIdleState : PlayerState
 {
-	// TODO:
-	// - [ ] Add in aiming with mouse and right stick
-	// - [ ] Make a testing character and tune to that and the prototype tileset
-
-
     public override void OnEnter(string prev_state)
     {
         DebugEnter(prev_state);
