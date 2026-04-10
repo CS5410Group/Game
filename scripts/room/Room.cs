8004dcd27f61879f9a53a8b2c98c7f3d128d84d3
sackofcookies <tommy.lesueur.temp@outlook.com>
2026-04-10 15:57:28 -0600

    yea

--- scripts/room/Room.cs
+++ scripts/room/Room.cs
@@ -4,38 +4,34 @@ using System;
 
 public partial class Room : Node2D
 {
-	Area2D CameraBounds;
 	CharacterBody2D Player;
 	Camera2D Camera;
+	TileMapLayer Ground;
+	float LimitLeft;
+	float LimitRight;
+	float LimitTop;
+	float LimitBottom;
+	[Export]
+	Rect2I CameraBounds;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		CameraBounds = GetNode<Area2D>("CameraBounds");
 		Player = GetNode<CharacterBody2D>("Player");
 		Camera = GetNode<Camera2D>("Camera");
+		Ground = GetNode<TileMapLayer>("Ground");
+		int tileSize = (int)Ground.TileSet.TileSize.X;
+
+		LimitLeft = CameraBounds.Position.X * tileSize;
+		LimitTop = CameraBounds.Position.Y * tileSize;
+		LimitRight = CameraBounds.End.X * tileSize;
+		LimitBottom = CameraBounds.End.Y * tileSize;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Player.MoveLocalX(1);
-		if (CameraBounds.OverlapsBody(Player)){
-			Camera.Position = Player.Position;
-			GD.Print("Player in bounds");
-		}
-		else {
-			var query = PhysicsRayQueryParameters2D.Create(Player.Position, new(0,0));
-			query.CollisionMask = 8;
-			query.CollideWithAreas = true;
-			Dictionary intersections = GetWorld2D().DirectSpaceState.IntersectRay(query);
-			GD.Print("Player out of bounds");
-			if (intersections.Count > 0){
-				Camera.Position = (Vector2)intersections["position"];
-			}
-			else {
-				GD.Print("some kinda error");
-				Camera.Position = new(0,0);
-			}
-		}
+		Camera.Position = new(Math.Clamp(Player.Position.X, LimitLeft, LimitRight), Math.Clamp(Player.Position.Y, LimitTop, LimitBottom));
+		GD.Print(Camera.Position.X + " " + Camera.Position.Y);
+		
 	}
 }
